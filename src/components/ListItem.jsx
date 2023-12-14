b46b3cfd6bc8fcf25b7bae437f9055156ba93a92
mirJakub <slawomir.krzyzak@gmail.com>
2023-12-14 18:39:06 +0100

    added listingitem

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "../styles/list.module.css";
+
+function ListingItem({ obj }) {
+  return (
+    <div className={classes.listItem} key={obj.id}>
+      {/* Render your item content here */}
+      <h3>{obj.title}</h3>
+      <p>{obj.description}</p>
+    </div>
+  );
+}
+
+export default ListingItem;
