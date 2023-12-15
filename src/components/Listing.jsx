c7caf57f4378d7975c1fea216733a70ad7d8e7e7
sonali582000 <rathodsonali393@gmail.com>
2023-12-15 17:48:10 +0100

    Made some changes in Listing

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,9 +1,18 @@
 import classes from "../styles/list.module.css";
+import rental from "../assets/rentals.json";
+import { useState } from "react";
 
 const Listing = () => {
   return (
     <>
-      <div className={classes.container}>List Comp</div>
+      console.log({rental.results[0].city})
+      <div className={classes.container}>
+        <div>
+          {rental.results.map((items) => {
+            return <div key={items.id}>{items.id}</div>;
+          })}
+        </div>
+      </div>
     </>
   );
 };
