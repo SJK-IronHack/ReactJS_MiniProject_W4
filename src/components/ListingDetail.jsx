c16290bdcdbafac043f7a568d1b3d4cb58b058b2
mirJakub <slawomir.krzyzak@gmail.com>
2023-12-16 20:50:01 +0100

    listing detail, scss

--- src/components/ListingDetail.jsx
+++ src/components/ListingDetail.jsx
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom";
 import Rentals from "../assets/rentals.json";
+import classes from "../styles/listingDetail.module.css";
+import '../styles/listingDetail.module.scss'
+
 
 const ListingDetail = () => {
   //const itemId = "6221624";
@@ -7,7 +10,39 @@ const ListingDetail = () => {
   const itemProfile = Rentals.results.find((item) => item.id == hotelId);
   return (
     <div>
-      <img src={itemProfile.picture_url.url} height={300} width={300} />
+      <div className={classes.itemDescription}>
+        <h3> {itemProfile.name}</h3>
+        <img src={itemProfile.picture_url.url} height={300} width={300} />
+
+        <div className={classes.infoBlat} >
+          <p>Price: {itemProfile.price}</p>
+          <p>Country: {itemProfile.country}</p>
+          <p>City: {itemProfile.city}</p>
+          <p>Host: {itemProfile.host_name}</p>
+          <p>Since: {itemProfile.host_since}</p>
+          <p>responding: {itemProfile.host_response_time}</p>
+          <p>Property type: {itemProfile.property_type}</p>
+          <p>Room type: {itemProfile.room_type}</p>
+
+        </div>
+
+        <div className={classes.description}>
+          <p className={classes.subHeadline}>Description</p>
+          <p> {itemProfile.description}</p>
+        </div>
+
+        <div className={classes.space}>
+          <p className={classes.subHeadline}>Space</p>
+          <p className={classes.space}> {itemProfile.space}</p>
+          <p> {itemProfile.description}</p>
+        </div>
+
+        <div className={classes.space}>
+          <p className={classes.subHeadline}>Rules</p>
+          <p className={classes.houseRules}> {itemProfile.house_rules}</p>
+        </div>
+
+      </div>
     </div>
   );
 };
